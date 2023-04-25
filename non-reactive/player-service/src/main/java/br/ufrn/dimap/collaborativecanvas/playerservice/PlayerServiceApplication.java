f0f56366036fc2afc76c07c1c6adc4ad25cf3290
jopak19 <jpaulogomessiqueira@gmail.com>
2023-04-25 19:20:37 -0300

    comit inicial

---
+++ non-reactive/player-service/src/main/java/br/ufrn/dimap/collaborativecanvas/playerservice/PlayerServiceApplication.java
@@ -0,0 +1,13 @@
+package br.ufrn.dimap.collaborativecanvas.playerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayerServiceApplication.class, args);
+	}
+
+}
