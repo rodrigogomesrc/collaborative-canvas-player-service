38a4832ad59ff8f23fddd2736861f44540306458
Rodrigo Rocha <rocha.rodrigogomes@gmail.com>
2023-05-18 17:01:31 -0300

    adiciona config para virtual threads

--- non-reactive/player-service/src/main/java/br/ufrn/dimap/collaborativecanvas/playerservice/config/ThreadsConfig.java
+++ non-reactive/player-service/src/main/java/br/ufrn/dimap/collaborativecanvas/playerservice/config/ThreadsConfig.java
@@ -1,13 +1,23 @@
 package br.ufrn.dimap.collaborativecanvas.playerservice.config;
 
+import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration;
 import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.core.task.support.TaskExecutorAdapter;
+
 import java.util.concurrent.Executors;
 
 @Configuration
 public class ThreadsConfig {
+
+    @Conditional(UseVirtualThreadCondition.class)
+    @Bean(TaskExecutionAutoConfiguration.APPLICATION_TASK_EXECUTOR_BEAN_NAME)
+    public AsyncTaskExecutor asyncTaskExecutor() {
+        return new TaskExecutorAdapter(Executors.newVirtualThreadPerTaskExecutor());
+    }
 
     @Conditional(UseVirtualThreadCondition.class)
     @Bean
