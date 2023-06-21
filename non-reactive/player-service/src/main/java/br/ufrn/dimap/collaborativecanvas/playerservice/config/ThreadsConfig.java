f0e3b791baa2ab80a71b76b9f5d3c8083d0af5e2
jopak19 <jpaulogomessiqueira@gmail.com>
2023-06-21 20:39:31 -0300

    configuração correta de thread virtual por properties

--- non-reactive/player-service/src/main/java/br/ufrn/dimap/collaborativecanvas/playerservice/config/ThreadsConfig.java
+++ non-reactive/player-service/src/main/java/br/ufrn/dimap/collaborativecanvas/playerservice/config/ThreadsConfig.java
@@ -1,5 +1,6 @@
 package br.ufrn.dimap.collaborativecanvas.playerservice.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration;
 import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
 import org.springframework.context.annotation.Bean;
@@ -13,13 +14,14 @@ import java.util.concurrent.Executors;
 @Configuration
 public class ThreadsConfig {
 
-    @Conditional(UseVirtualThreadCondition.class)
+	@ConditionalOnProperty(prefix = "app", name= "use-virtual-threads")
     @Bean(TaskExecutionAutoConfiguration.APPLICATION_TASK_EXECUTOR_BEAN_NAME)
     public AsyncTaskExecutor asyncTaskExecutor() {
+		System.out.println("=============== USING VIRTUAL THREADS ===========");
         return new TaskExecutorAdapter(Executors.newVirtualThreadPerTaskExecutor());
     }
 
-    @Conditional(UseVirtualThreadCondition.class)
+	@ConditionalOnProperty(prefix = "app", name= "use-virtual-threads")
     @Bean
     public TomcatProtocolHandlerCustomizer<?> protocolHandlerVirtualThreadExecutorCustomizer() {
         return protocolHandler -> {
