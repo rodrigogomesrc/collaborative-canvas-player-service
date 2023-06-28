9610bbd77bba5e08968513dd2a1a220b262e82d5
jopak19 <jpaulogomessiqueira@gmail.com>
2023-06-28 10:21:25 -0300

    habilita discovery client

--- non-reactive/player-service/src/main/java/br/ufrn/dimap/collaborativecanvas/playerservice/PlayerServiceApplication.java
+++ non-reactive/player-service/src/main/java/br/ufrn/dimap/collaborativecanvas/playerservice/PlayerServiceApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.boot.autoconfigure.domain.EntityScan;
 //import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 //@EntityScan("br.ufrn.dimap.collaborativecanvas.playerservice.models")
 //@EnableJpaRepositories(basePackages = "br.ufrn.dimap.collaborativecanvas.playerservice.repository")
+@EnableDiscoveryClient
 @SpringBootApplication
 public class PlayerServiceApplication {
 
