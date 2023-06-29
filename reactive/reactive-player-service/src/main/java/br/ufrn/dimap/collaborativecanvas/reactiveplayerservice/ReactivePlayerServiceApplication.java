9e324df2d1799690dde9cc1d4a660b9381715e73
jopak19 <jpaulogomessiqueira@gmail.com>
2023-06-28 21:11:43 -0300

    configuração de gateway e discovery

--- reactive/reactive-player-service/src/main/java/br/ufrn/dimap/collaborativecanvas/reactiveplayerservice/ReactivePlayerServiceApplication.java
+++ reactive/reactive-player-service/src/main/java/br/ufrn/dimap/collaborativecanvas/reactiveplayerservice/ReactivePlayerServiceApplication.java
@@ -1,8 +1,11 @@
 package br.ufrn.dimap.collaborativecanvas.reactiveplayerservice;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ReactivePlayerServiceApplication {
 
