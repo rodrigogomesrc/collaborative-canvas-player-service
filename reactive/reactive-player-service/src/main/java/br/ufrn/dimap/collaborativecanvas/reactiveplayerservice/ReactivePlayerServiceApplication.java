4860846a19243a72b9d065ea493b04e0b91e1ea6
Rodrigo Rocha <rocha.rodrigogomes@gmail.com>
2023-07-08 17:47:38 -0300

    teste adição de mensageria

--- reactive/reactive-player-service/src/main/java/br/ufrn/dimap/collaborativecanvas/reactiveplayerservice/ReactivePlayerServiceApplication.java
+++ reactive/reactive-player-service/src/main/java/br/ufrn/dimap/collaborativecanvas/reactiveplayerservice/ReactivePlayerServiceApplication.java
@@ -3,11 +3,17 @@ package br.ufrn.dimap.collaborativecanvas.reactiveplayerservice;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
-@EnableDiscoveryClient
+import java.util.function.Consumer;
+
 @SpringBootApplication
 public class ReactivePlayerServiceApplication {
+
+	@Bean
+	public Consumer<String> printhelloworld() {
+		return val -> System.out.println(val);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReactivePlayerServiceApplication.class, args);
